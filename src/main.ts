1415dc0f2b24639cad861b14d5a8842c2380aa2e
Katryel Menezes <katryelmenezesvieira14@gmail.com>
2024-12-30 18:20:52 -0300

    Adiciona interceptores de conflito e de banco de dados, além de manipulação de erros específicos

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConflictInterceptor } from './common/errors/interceptors/conflict.interceptor';
+import { DatabaseInterceptor } from './common/errors/interceptors/database.interceptor';
 import { NotFoundInterceptor } from './common/errors/interceptors/notfound.interceptor';
 import { UnauthorizedInterceptor } from './common/errors/interceptors/unauthorized.interceptor';
-import { DatabaseInterceptor } from './common/errors/interceptors/database.interceptor';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
@@ -15,9 +16,10 @@ async function bootstrap() {
         }),
     );
     //app.useGlobalFilters(new HttpExceptionFilter());
-    app.useGlobalInterceptors(new NotFoundInterceptor());
-    app.useGlobalInterceptors(new UnauthorizedInterceptor());
+    app.useGlobalInterceptors(new ConflictInterceptor());
     app.useGlobalInterceptors(new DatabaseInterceptor());
+    app.useGlobalInterceptors(new UnauthorizedInterceptor());
+    app.useGlobalInterceptors(new NotFoundInterceptor());
     await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
