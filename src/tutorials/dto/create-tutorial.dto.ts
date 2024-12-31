ec98aa657a9978545922f947a587802e7ddf7c5b
Katryel Menezes <katryelmenezesvieira14@gmail.com>
2024-12-31 17:00:11 -0300

    Atualiza repositório de tutoriais, adiciona validações ao DTO de criação e ajusta importações para o Swagger

--- src/tutorials/dto/create-tutorial.dto.ts
+++ src/tutorials/dto/create-tutorial.dto.ts
@@ -1,6 +1,17 @@
+import { IsNotEmpty, IsString, MinLength, MaxLength } from 'class-validator';
+
 export class CreateTutorialDto {
-    id: string;
+    @IsString()
+    @IsNotEmpty({ message: 'O Título é obrigatório' })
+    @MinLength(3, { message: 'O Título deve ter no mínimo 3 caracteres' })
+    @MaxLength(256, { message: 'O Título deve ter no máximo 256 caracteres' })
     title: string;
+
+    @IsString()
+    @IsNotEmpty({ message: 'A Descrição é obrigatória' })
+    @MinLength(3, { message: 'A Descrição deve ter no mínimo 3 caracteres' })
+    @MaxLength(1000, {
+        message: 'A Descrição deve ter no máximo 1000 caracteres',
+    })
     description: string;
-    user_id: string;
 }
--- src/tutorials/repositories/tutorial.repository.ts
+++ src/tutorials/repositories/tutorial.repository.ts
@@ -2,72 +2,11 @@
 
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { CreateTutorialDto } from '../dto/create-tutorial.dto';
-import { UpdateTutorialDto } from '../dto/update-tutorial.dto';
-import { Tutorial } from '../entities/tutorial.entity';
+// import { CreateTutorialDto } from '../dto/create-tutorial.dto';
+// import { UpdateTutorialDto } from '../dto/update-tutorial.dto';
+// import { Tutorial } from '../entities/tutorial.entity';
 
 @Injectable()
-export class UsersRepository {
+export class TutorialsRepository {
     constructor(private readonly prisma: PrismaService) {}
-
-    async create(createUserDto: CreateTutorialDto): Promise<Tutorial> {
-        const userAlreadyExists = await this.prisma.user.findFirst({
-            where: {
-                email: createUserDto.email,
-            },
-        });
-
-        if (userAlreadyExists) {
-            return null;
-        }
-
-        return await this.prisma.user.create({
-            data: {
-                ...createUserDto,
-            },
-        });
-    }
-
-    async findAll(): Promise<Tutorial[]> {
-        return await this.prisma.user.findMany({
-            where: {
-                delete_att: null,
-            },
-        });
-    }
-
-    async findOne(id: string): Promise<Tutorial> {
-        return await this.prisma.user.findUnique({
-            where: {
-                id,
-                delete_att: null,
-            },
-        });
-    }
-
-    async update(id: string, updateUserDto: UpdateTutorialDto): Promise<Tutorial> {
-        return await this.prisma.user.update({
-            where: {
-                id,
-                delete_att: null,
-            },
-            data: {
-                ...updateUserDto,
-                update_att: new Date(),
-            },
-        });
-    }
-
-    async remove(id: string): Promise<{ message: string }> {
-        await this.prisma.user.update({
-            where: {
-                id,
-                delete_att: null,
-            },
-            data: {
-                delete_att: new Date(),
-            },
-        });
-        return { message: 'Usuário removido com sucesso' };
-    }
 }
