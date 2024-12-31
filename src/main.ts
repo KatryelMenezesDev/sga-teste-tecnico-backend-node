d3b91a70accf898c70f728dafbb0f90018f5e868
Katryel Menezes <katryelmenezesvieira14@gmail.com>
2024-12-31 16:18:56 -0300

    Adiciona documentação Swagger à API e configura o endpoint para acesso

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { DatabaseInterceptor } from './common/errors/interceptors/database.inter
 import { NotFoundInterceptor } from './common/errors/interceptors/notfound.interceptor';
 import { UnauthorizedInterceptor } from './common/errors/interceptors/unauthorized.interceptor';
 import { BadRequestInterceptor } from './common/errors/interceptors/badrequest.interceptor';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
@@ -22,6 +23,17 @@ async function bootstrap() {
     app.useGlobalInterceptors(new UnauthorizedInterceptor());
     app.useGlobalInterceptors(new NotFoundInterceptor());
     app.useGlobalInterceptors(new BadRequestInterceptor());
+
+    const config = new DocumentBuilder()
+        .setTitle('SGA Challenge API')
+        .setDescription(
+            'Este projeto foi desenvolvido como parte de um teste técnico para a posição de Desenvolvedor(a) Backend. O objetivo principal é demonstrar habilidades em desenvolvimento backend utilizando Node.js, TypeScript, e NestJS, além de boas práticas de codificação, como escrita de código limpo, escalável e bem estruturado.',
+        )
+        .setVersion('1.0')
+        .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api-docs', app, document);
+
     await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
