5c6a05b9cca5a8cd0300f8f882a80e1914230069
Katryel Menezes <katryelmenezesvieira14@gmail.com>
2025-01-02 19:54:20 -0300

    Adiciona proteção de rota com AuthGuard JWT ao método findAll no UsersController

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,12 +6,14 @@ import {
     Patch,
     Param,
     Delete,
+    UseGuards,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { AuthUserDto } from './dto/auth-user.dto';
 import { ApiForbiddenResponse, ApiResponse } from '@nestjs/swagger';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('users')
 export class UsersController {
@@ -25,6 +27,7 @@ export class UsersController {
     }
 
     @Get()
+    @UseGuards(AuthGuard('jwt'))
     findAll() {
         return this.usersService.findAll();
     }
