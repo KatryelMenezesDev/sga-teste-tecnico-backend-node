ace65c52f8cad75719944613b5515414f78a9dc0
Katryel Menezes <katryelmenezesvieira14@gmail.com>
2025-01-02 14:49:39 -0300

    Implementa validação de usuário e geração de token JWT no AuthService

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        private readonly usersService: UsersService,
+        private readonly jwtService: JwtService,
+    ) {}
+
+    async validateUser(id: string): Promise<any> {
+        const user = await this.usersService.findOne(id);
+
+        if (user) {
+            return user;
+        }
+    }
+
+    async auth(user: any) {
+        const payload = { id: user.id, email: user.email };
+        return {
+            access_token: this.jwtService.sign(payload),
+        };
+    }
+}
